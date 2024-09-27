fcc225d669674461b2af48dc94c335d379be8b75
Col-13 <henrydelcol94@gmail.com>
2024-09-27 11:42:16 -0300

    arquivo banco de dados criado e scripts mudados dentro do arquivo servidor. vou pesquisar uma nova dependencia para eliminar erro que ocorreu.

--- projetos/api-com-express/src/servidor.js
+++ projetos/api-com-express/src/servidor.js
@@ -2,9 +2,21 @@ const porta = 3003
 
 const express = require('express')
 const app = express()
-
+const bancoDeDados = require('./banco-de-dados')
 app.get('/produtos', (req, res, next) => {
-    res.send({nome: 'ventilador', preco: 203.54})
+    res.send(bancoDeDados.getProdutos())
+})
+
+app.get('/produtos/:id', (req, res, next) => {
+    res.send(bancoDeDados.getProduto(req.params.id))
+})
+
+app.post('/produtos', (req, res, next) => {
+    const produto = bancoDeDados.salvarProduto({
+        nome: req.body.name,
+        preco: req.body.preco
+    })
+    res.send(produto)
 })
 
 app.listen(porta, () => {
